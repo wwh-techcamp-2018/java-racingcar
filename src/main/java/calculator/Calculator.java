8bfc828be45b89537b114f3325a7ca65526ce6ed
YeonTaeKIM <urbanscenery@gmail.com>
2018-07-02 17:52:35 +0900

    8조 문자열 계산기@이성근

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,20 @@
 package calculator;
 
 public class Calculator {
-    int plus(int a, int b){
+
+    public int plus(int a, int b){
         return a + b;
     }
-    int minus(int a, int b){
+
+    public int minus(int a, int b){
         return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
     }
 }
