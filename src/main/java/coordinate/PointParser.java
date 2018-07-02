bcca3cf72a99ae373b3a3a9441aaba99a89782c7
sheep1500 <cjswotl6274@naver.com>
2018-07-02 18:06:25 +0900

    test completed (#14)

---
+++ src/main/java/coordinate/PointParser.java
@@ -0,0 +1,16 @@
+package coordinate;
+
+public class PointParser {
+    static int[] parse(String text){
+        String escape = text.substring(1,text.length()-1);
+        String[] values = escape.split(",");
+        int[] numbers = new int[values.length];
+        return null;
+    }
+
+    static int[] toInt(String a){
+        return null;
+    }
+
+
+}
