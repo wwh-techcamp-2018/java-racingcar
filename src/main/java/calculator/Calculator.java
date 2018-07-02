f12dc485bbca6a0bb39d1bc016c38137d75d3361
우아테크캠프_19 <woowatechcamp_19@woowatechcamp19.local>
2018-07-02 19:02:17 +0900

    Finish string calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,31 +1,52 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
 
     public static String[] parse(String text) {
+        if (text == null || text == " ") {
+            throw new IllegalArgumentException();
+
+        }
         return text.split(" ");
     }
 
-    static int calculate(String[] arr) {
-        String[] numbers = new String[2];
+    public static int calculate(String[] arr) {
+        int result = Integer.parseInt(arr[0]);
 
-        for(int i=0; i < arr.length; i++) {
-            int number = Integer.parseInt(arr[i]);
-
-            switch (arr[i]) {
-                case "+":
-                    break;
-                case "-":
-                    break;
-                case "/":
-                    break;
-                case "*":
-                    break;
-            }
+        for(int i=1; i < arr.length; i+=2) {
+            int temp = Integer.parseInt(arr[i + 1]);
+            result = calculate(result, temp, arr[i]);
         }
+
+        return result;
+    }
+
+    private static int calculate(int result, int temp, String operator) {
+        if (operator.equals("+")) {
+            return result + temp;
+        }
+
+        if (operator.equals("-")) {
+            return result - temp;
+        }
+
+        if (operator.equals("*")) {
+            return result * temp;
+        }
+
+        if (operator.equals("/")) {
+            return result / temp;
+        }
+
+        throw new ArithmeticException();
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
 
+        System.out.println(Calculator.calculate(Calculator.parse(value)));
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -15,4 +15,51 @@ public class CalculatorTest {
         assertEquals(arr[3],"*");
         assertEquals(arr[4],"4");
     }
+
+    @Test
+    public void plus() {
+        String[] arr = {"2", "+", "3"};
+        assertEquals(5, Calculator.calculate(arr));
+    }
+
+    @Test
+    public void minus() {
+        String[] arr = {"2", "-", "4"};
+        assertEquals(-2, Calculator.calculate(arr));
+    }
+
+    @Test
+    public void multiple() {
+        String[] arr = {"-2", "*", "3"};
+        assertEquals(-6, Calculator.calculate(arr));
+    }
+
+    @Test
+    public void divide() {
+        String[] arr = {"4", "/", "3"};
+        assertEquals(1, Calculator.calculate(arr));
+    }
+
+    @Test (expected = NumberFormatException.class)
+    public void calculateNotNumber() {
+        String[] arr = {"2", "+", "a"};
+        Calculator.calculate(arr);
+    }
+
+    @Test (expected = ArithmeticException.class)
+    public void calculateNotOperator() {
+        String[] arr = {"2", ")", "3"};
+        Calculator.calculate(arr);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void inputStringSpace() {
+        Calculator.parse(" ");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void inputStringNull() {
+        Calculator.parse(null);
+    }
+
 }
