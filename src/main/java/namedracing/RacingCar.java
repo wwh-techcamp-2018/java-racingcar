77ea02c56ddc6785d0eebdbc1407e84af96462ee
우아테크캠프_7 <woowatechcamp_7@woowatechcamp7.local>
2018-07-03 17:06:47 +0900

    removed unnecessary variables

--- src/main/java/namedracing/RacingCar.java
+++ src/main/java/namedracing/RacingCar.java
@@ -10,11 +10,10 @@ public class RacingCar {
     private final int MAX_BOUND = 10;
 
 
-    List<Car> cars = new ArrayList<Car>();
+    List<Car> cars = new ArrayList<>();
     int times;
 
     Scanner scanner = new Scanner(System.in);
-    int maxDistance = 0;
 
     void getInput() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
