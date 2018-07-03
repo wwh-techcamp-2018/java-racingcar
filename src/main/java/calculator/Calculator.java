e1b0e854a783b923f20c3a02cef8dea853204f15
우아테크캠프_12 <woowatechcamp_12@woowatechcamp12.local>
2018-07-03 09:58:20 +0900

    Step0 calculator 클래스를 완성하였습니다.
    1. caculator 클래스는 한 줄 입력을 받아 계산해주는 클래스입니다.
    2. plus, minus, multiply, divide, 그리고 입력에 대한 에러 테스트를 진행하였습니다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,47 +4,50 @@ import java.util.Scanner;
 
 public class Calculator {
     Scanner scanner = new Scanner(System.in);
-    String inputString = scanner.nextLine();
-    private int plus (int a, int b) {
+
+    public int plus(int a, int b) {
         return a + b;
     }
 
-    private int minus (int a, int b) {
+    public int minus(int a, int b) {
         return a - b;
     }
 
-    private int multiply (int a, int b) {
+    public int multiply(int a, int b) {
         return a * b;
     }
 
-    private int divide (int a, int b) {
+    public int divide(int a, int b) {
         return a / b;
     }
 
-    public int calculate() {
+    public int calculate(String inputString) {
         String[] splitted = inputString.split(" ");
-        int tempInt=0;
-
-        for(int i = 0 ; i < splitted.length - 2; i+=2){
-            //int tempInt = Integer.parseInt(splitted[i]) + Integer.parseInt(splitted[i+2]);
-            tempInt = multyplexCalc(splitted,i);
+        if (splitted.length < 3) {
+            throw new IllegalArgumentException();
         }
+
+        int tempInt = 0;
+        for (int i = 0; i < splitted.length - 2; i += 2) {
+            tempInt = multiplexCalc(splitted, i);
+            splitted[i + 2] = Integer.toString(tempInt);
+        }
+
         return tempInt;
     }
 
-    private int multyplexCalc(String[] splitted, int i){
-        switch (splitted[i+1]){
-            case "+":
-                return plus(Integer.parseInt(splitted[i]),Integer.parseInt(splitted[i+2]));
-            case "-":
-                return minus(Integer.parseInt(splitted[i]),Integer.parseInt(splitted[i+2]));
-            case "*":
-                return multiply(Integer.parseInt(splitted[i]),Integer.parseInt(splitted[i+2]));
-            case "/":
-                return divide(Integer.parseInt(splitted[i]),Integer.parseInt(splitted[i+2]));
-
+    private int multiplexCalc(String[] splitted, int i) {
+        switch (splitted[i + 1].charAt(0)) {
+            case '+':
+                return plus(Integer.parseInt(splitted[i]), Integer.parseInt(splitted[i + 2]));
+            case '-':
+                return minus(Integer.parseInt(splitted[i]), Integer.parseInt(splitted[i + 2]));
+            case '*':
+                return multiply(Integer.parseInt(splitted[i]), Integer.parseInt(splitted[i + 2]));
+            case '/':
+                return divide(Integer.parseInt(splitted[i]), Integer.parseInt(splitted[i + 2]));
+            default:
+                throw new IllegalArgumentException();
         }
     }
-
-
 }
